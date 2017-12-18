3a89b731630e98c73e6ac900e967481f18c6e4fe
Martine Eisinga <eisingamartine@gmail.com>
2017-12-18 18:16:01 +0100

    changes in showpage of trip - add activities etc

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171218104439) do
+ActiveRecord::Schema.define(version: 20171218163451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 20171218104439) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "hotel_name"
     t.index ["category_id"], name: "index_trips_on_category_id"
     t.index ["user_id"], name: "index_trips_on_user_id"
   end
